63367d1db628a0de2c55e6ed9bc92b285f542eac
Jason Derrett <jason.derrett@gmail.com>
2023-11-04 21:20:51 +0000

    gitpodding

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Choo Choo! This is an example of a create-react-app site running on Railway.</p>
+        <p>Choo Choo! This is an example of a create-react-app site running on Railway, and I hate it.</p>
         <a className="App-link" href="https://react.dev/learn" target="_blank" rel="noreferrer noopener">Learn React</a>
       </header>
     </div>
